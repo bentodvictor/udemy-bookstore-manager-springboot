1898367f08fc31c912b4f67959106a3971bfe060
da3mons <victordallbento@hotmail.com>
2020-07-25 23:53:39 -0300

    Created book repository class

---
+++ src/main/java/com/da3mons/bookstoremanager/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.da3mons.bookstoremanager.repository
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long>{
+    
+}
